3bf7c31184782831d6aa4c19939c5054ff267b5a
Dipannita Shil <dipannitashil24@gmail.com>
2021-02-14 14:38:34 +0530

    cloud config server

---
+++ spring-cloud-config-server/src/test/java/dipannita/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dipannita.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
